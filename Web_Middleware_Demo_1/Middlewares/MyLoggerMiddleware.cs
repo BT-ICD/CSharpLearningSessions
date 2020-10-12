1630b80deb53bdb558bc579e83047833fd14a8e7
Bhavin Trivedi <dearbhavin@hotmail.com>
2020-10-12 09:26:54 +0530

    Created project Web_Middleware_Demo_1 to learn about custom middleware.

---
+++ Web_Middleware_Demo_1/Middlewares/MyLoggerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Middleware_Demo_1.Middlewares
+{
+    public class MyLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILoggerFactory _loggerFactory;
+        public MyLoggerMiddleware(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _loggerFactory = loggerFactory;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var logger = _loggerFactory.CreateLogger<MyLoggerMiddleware>();
+            try
+            {
+                logger.LogInformation("Performing Middleware Operation - Entering Middleware ");
+                logger.LogInformation("Context.Request.Host.Value " + context.Request.Host.Value);
+                logger.LogInformation("Context.Request.Path.Value " + context.Request.Path.Value);
+                
+                // await context.Response.WriteAsync( "Short Circuit!");
+                await _next(context);
+                logger.LogInformation("Performing Middleware Operation - Exit Middleware ");
+            }
+            catch (Exception ex) 
+            {
+                logger.LogError($"Something went wrong: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Web_Middleware_Demo_1/Middlewares/MyLoggerMiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Middleware_Demo_1.Middlewares
+{
+    public static class MyLoggerMiddlewareExtension
+    {
+        public static IApplicationBuilder UseMyLoggerMiddleware(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<MyLoggerMiddleware>();
+        }
+    }
+}

c79aa1403bb4573c09ef36aeb6de48cfce9ab637
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-02-07 22:23:41 +0530

    Expression bodied Members and params to pass variable number of arguments demo

---
+++ Ex_UserDefinedClass01/MyTime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex_UserDefinedClass01
+{
+    public class MyTime
+    {
+        public int Seconds { get; set; }
+        public int Minutes { get; set; }
+
+        /// <summary>
+        /// Example of Expression Bodied Members
+        /// Reference - https://docs.microsoft.com/en-us/dotnet/csharp/methods#expression-bodied-members
+        /// </summary>
+        /// <returns></returns>
+        public int GetTimeInSeconds() => ((Minutes * 60) + Seconds);
+             
+    }
+}
--- Ex_UserDefinedClass01/Program.cs
+++ Ex_UserDefinedClass01/Program.cs
@@ -6,10 +6,21 @@ namespace Ex_UserDefinedClass01
     {
         static void Main(string[] args)
         {
-            SquareVer1 s1 = new SquareVer1();
-            s1.length = 10;
-            int area = s1.Area();
-            Console.WriteLine($"Area is {area}");
+            //Example of params to pass variable number of arguments
+            int sum = addition(10, 20,30);
+            Console.WriteLine($"Sum of values: {sum}");
+            //Example of Expression Bodied Members
+            //MyTime t1 = new MyTime();
+            //t1.Minutes = 1;
+            //t1.Seconds = 20;
+            //int totalSeconds = t1.GetTimeInSeconds();
+            //Console.WriteLine($"Total seconds of time t1 is: {totalSeconds}");
+
+            //SquareVer1 s1 = new SquareVer1();
+            //s1.length = 10;
+            //int area = s1.Area();
+            //Console.WriteLine($"Area is {area}");
+
 
             //First Demo of User Defined class 
             //Console.WriteLine("Hello World!");
@@ -27,6 +38,21 @@ namespace Ex_UserDefinedClass01
             //{
             //    Console.WriteLine(item);
             //}
+        }
+        /// <summary>
+        /// Example of params to get variable number of arguments
+        /// Reference: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/params
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        static int addition(params int[] values)
+        {
+            int result = 0;
+            foreach(var data in values)
+            {
+                result += data;
+            }
+            return result;
         }
         /// <summary>
         /// To get first Name, Middle Name and Last Name as an array of string

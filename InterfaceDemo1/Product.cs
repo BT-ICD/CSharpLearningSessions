f554de1e587a140ee11733a426a609e305071708
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-03-17 10:20:48 +0530

    Example of IComparable interface

--- InterfaceDemo1/Product.cs
+++ InterfaceDemo1/Product.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace InterfaceDemo1
 {
-    public class Product:IComparable
+    public class Product : IComparable
     {
         public int Id { get; set; }
         public string Name { get; set; }
@@ -19,6 +19,10 @@ namespace InterfaceDemo1
                 return (this.Rate - otherProduct.Rate);
             }
             return 1;
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Rate: {Rate}";
         }
     }
 }
--- InterfaceDemo1/Program.cs
+++ InterfaceDemo1/Program.cs
@@ -7,19 +7,35 @@ namespace InterfaceDemo1
         static void Main(string[] args)
         {
             Console.WriteLine("Example of IComparable Interface!");
-            Product p1 = new Product() { Id = 101, Name = "Dettol", Rate = 30 };
-            Console.WriteLine(typeof(Product).IsInstanceOfType(p1));
-            Console.WriteLine(typeof(IComparable).IsInstanceOfType(p1));
+            Product[] products = new Product[4];
+            products[0] = new Product() { Id = 101, Name = "Dettol", Rate = 30 };
+            products[1] = new Product() { Id = 102, Name = "Cinthol", Rate = 20 };
+            products[2] = new Product() { Id = 103, Name = "Hamam", Rate = 10 };
+            products[3] = new Product() { Id = 104, Name = "Dove", Rate = 45 };
 
-            int x = 10;
-            Console.WriteLine(x is int);
-            Console.WriteLine(x is long);
-            Object b1 = new Base();
-            Object d1 = new Derived();
-            Console.WriteLine(b1 is Base);
-            Console.WriteLine(d1 is Derived);
-            Console.WriteLine(b1 is Derived);
+            //Console.WriteLine(typeof(Product).IsInstanceOfType(p1));
+            //Console.WriteLine(typeof(IComparable).IsInstanceOfType(p1));
 
+            //int x = 10;
+            //Console.WriteLine(x is int);
+            //Console.WriteLine(x is long);
+            //Object b1 = new Base();
+            //Object d1 = new Derived();
+            //Console.WriteLine(b1 is Base);
+            //Console.WriteLine(d1 is Derived);
+            //Console.WriteLine(b1 is Derived);
+
+            Console.WriteLine("Prior to sort");
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
+            Array.Sort(products);
+            Console.WriteLine("After to sort");
+            foreach (var item in products)
+            {
+                Console.WriteLine(item);
+            }
 
         }
         

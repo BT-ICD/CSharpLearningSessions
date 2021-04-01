0b9b794cb0afb9177ad68bfa1a9f01c389ca7702
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-04-01 21:00:19 +0530

    Created view for Razor Syntax demo

---
+++ WebApplicationMVC1/Controllers/RazorDemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC1.Controllers
+{
+    public class RazorDemoController : Controller
+    {
+        /// <summary>
+        /// To understand and learn about Razor syntax
+        /// Learning reference: https://docs.microsoft.com/en-us/aspnet/core/mvc/views/razor?view=aspnetcore-5.0
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

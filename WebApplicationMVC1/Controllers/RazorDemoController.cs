c9f5663a877081a845672592357bcc53389628e1
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-04-07 10:25:37 +0530

    Pass data using ViewData.

--- WebApplicationMVC1/Controllers/RazorDemoController.cs
+++ WebApplicationMVC1/Controllers/RazorDemoController.cs
@@ -3,11 +3,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplicationMVC1.Models;
 
 namespace WebApplicationMVC1.Controllers
 {
     public class RazorDemoController : Controller
     {
+        //ViewData Attribute = Properties on controllers or Razor Page models marked with the [ViewData] attribute have their values stored and loaded from the dictionary.
+        [ViewData]
+        public string MessageData { get; set; }
         /// <summary>
         /// To understand and learn about Razor syntax
         /// Learning reference: https://docs.microsoft.com/en-us/aspnet/core/mvc/views/razor?view=aspnetcore-5.0
@@ -15,6 +19,21 @@ namespace WebApplicationMVC1.Controllers
         /// <returns></returns>
         public IActionResult Index()
         {
+            return View();
+        }
+        /// <summary>
+        /// Method with argument where user can pass data using query string
+        /// View can access value using viewData
+        /// </summary>
+        /// <param name="numTimes"></param>
+        /// <returns></returns>
+        public IActionResult Names(string name, int numTimes)
+        {
+            Address address = new Address() { Area = "Naranpura", City = "Ahmedabad", State = "Gujarat" };
+            MessageData = "Hello World, thanks to ViewData attribute";
+            ViewData["numTimes"] = numTimes;
+            ViewData.Add("name", name);
+            ViewData["address"] = address;
             return View();
         }
     }
---
+++ WebApplicationMVC1/Models/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC1.Models
+{
+    public class Address
+    {
+        public string Area { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}

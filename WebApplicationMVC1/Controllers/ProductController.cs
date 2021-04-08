8139b1b69c24b5ccb70608e165c4b27eaeb571f3
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-04-08 21:08:00 +0530

    Pass Model from Controller and FormData from View to Controller

--- WebApplicationMVC1/Controllers/ProductController.cs
+++ WebApplicationMVC1/Controllers/ProductController.cs
@@ -23,5 +23,24 @@ namespace WebApplicationMVC1.Controllers
             var list = Product.List();
             return View(list);
         }
+        public IActionResult Find()
+        {
+            return View("FindProduct");
+        }
+        /// <summary>
+        /// This method Display is acccesible as a get when we cal lfrom url with query string parameter
+        /// THis method Display is also accessible as a HTTPPost call when we Submit Form. Value of ProductId is accessible due to form paramter
+        /// Information is sent to the server as a form field value
+        /// References: 
+        /// https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/search?view=aspnetcore-5.0
+        /// https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_and_retrieving_form_data
+        /// </summary>
+        /// <param name="ProductId"></param>
+        /// <returns></returns>
+        public IActionResult Display(int ProductId)
+        {
+            var result = Product.List().Where(p => p.Id == ProductId).FirstOrDefault();
+            return View("View",result);
+        }
     }
 }

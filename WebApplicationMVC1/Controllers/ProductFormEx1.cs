adb34f85201ceee5d162cb52d7c5e52b4de135d8
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-04-19 10:06:31 +0530

    Pass data from view to controller using form example 1

---
+++ WebApplicationMVC1/Controllers/ProductFormEx1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationMVC1.Models;
+
+namespace WebApplicationMVC1.Controllers
+{
+    public class ProductFormEx1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(int Id, String Name, int Rate, String Category)
+        {
+            Product objData = new Product() { Id = Id, Name = Name , Rate=Rate, Category=Category};
+            return View();
+        }
+    }
+}

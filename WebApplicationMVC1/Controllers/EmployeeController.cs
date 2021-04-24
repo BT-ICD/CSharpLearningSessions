49dbdff4f6710afbe4fb20c43a6ff9fa8705d5c6
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-04-24 11:51:15 +0530

    select asp-item tag helper for dropdown

---
+++ WebApplicationMVC1/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationMVC1.Models;
+
+namespace WebApplicationMVC1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            List<SelectListItem> suffixListItems = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text="Mr", Value="1"},
+                new SelectListItem(){Text="Master", Value="2", Selected=true},
+                new SelectListItem(){Text="Miss", Value="3"},
+                new SelectListItem(){Text="Mrs", Value="4"}
+            };
+            ViewBag.suffixListItems = suffixListItems;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Employee employee)
+        {
+            IEnumerable<SelectListItem> suffixListItems = new List<SelectListItem>()
+            {
+                new SelectListItem(){Text="Mr", Value="1"},
+                new SelectListItem(){Text="Master", Value="2", Selected=true},
+                new SelectListItem(){Text="Miss", Value="3"},
+                new SelectListItem(){Text="Mrs", Value="4"}
+            };
+            ViewBag.suffixListItems = suffixListItems;
+            Employee obj = employee;
+            return View();
+        }
+
+    }
+}
---
+++ WebApplicationMVC1/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationMVC1.Models
+{
+    public class Employee
+    {
+        [DisplayName("Employee Id")]
+        public int Id { get; set; }
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [DisplayName("Gender")]
+        public string Gender { get; set; }
+
+        [DisplayName("Is Active")]
+        public bool IsActive { get; set; }
+
+        public bool IsNew { get; set; }
+        public DepartmentEnum Department { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Suffix { get; set; }
+    }
+}

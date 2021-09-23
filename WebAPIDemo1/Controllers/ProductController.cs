384f46494a777a83030efc3e75b6640ca129133f
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-09-23 20:02:39 +0530

    Web API Initial Demo Project for session 1. Routing

---
+++ WebAPIDemo1/Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo1.Models;
+
+namespace WebAPIDemo1.Controllers
+{
+    /// <summary>
+    /// Learning reference: https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/routing?view=aspnetcore-5.0#verb
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        /// <summary>
+        /// To get list of products
+        /// </summary>
+        /// <returns></returns>
+        /// Sample Call: https://localhost:44389/api/Product
+        [HttpGet]
+        public IActionResult List()
+        {
+            var result = Product.GetProductList();
+            return Ok(result);
+        }
+        /// <summary>
+        /// To get product by name. Using string as route parameter
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        /// Sample call: https://localhost:44389/api/Product/Cinthol
+        [HttpGet("{productName}")]
+        public IActionResult Get(string productName)
+        {
+            var result = Product.GetProductList().Where(obj => obj.Name == productName).FirstOrDefault();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+
+        }
+        /// <summary>
+        /// Id as a route parameter. To get particular product by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// Sample call: https://localhost:44389/api/Product/101
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var result = Product.GetProductList().Where(obj=>obj.Id==id).FirstOrDefault();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        /// <summary>
+        /// To get by rate. Second method to get data by int parameter.  
+        /// Added byrate in url to define access by rate
+        /// </summary>
+        /// <param name="rate"></param>
+        /// <returns></returns>
+        /// Sample call: https://localhost:44389/api/Product/byrate/50
+        [HttpGet("byrate/{rate}")]
+        public IActionResult GetByRate(int rate)
+        {
+            var result = Product.GetProductList().Where(obj => obj.Rate == rate).FirstOrDefault();
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+
+        }
+    }
+}
---
+++ WebAPIDemo1/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIDemo1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        /// <summary>
+        /// To get list of products
+        /// </summary>
+        /// <returns></returns>
+        public static List<Product> GetProductList()
+        {
+            var result = new List<Product>()
+            {
+                new Product(){Id=101,Name="Dettol", Rate=30},
+                new Product(){Id=102,Name="Cinthol", Rate=80},
+                new Product(){Id=103,Name="Pears", Rate=50},
+                new Product(){Id=104,Name="Dove", Rate=20},
+            };
+            return result;
+        }
+    }
+}

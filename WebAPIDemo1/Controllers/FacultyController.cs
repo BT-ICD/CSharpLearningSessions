9360a4b24a56a1f5099dfd5ce2e9d765d85c5536
Bhavin Trivedi <dearbhavin@hotmail.com>
2021-12-13 16:00:34 +0530

    Upload image using FromForm

---
+++ WebAPIDemo1/Controllers/FacultyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo1.Models;
+
+namespace WebAPIDemo1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FacultyController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Add([FromForm] Faculty faculty)
+        {
+            var locationToSaveFile = Path.Combine(AppContext.BaseDirectory, "Documents", faculty.ImageFile.FileName);
+
+            //document will stored under bin\debug\netcoreapp3.1\documents
+            var data = faculty;
+            using(FileStream stream = System.IO.File.Create(locationToSaveFile))
+            {
+                faculty.ImageFile.CopyTo(stream);
+            }
+            return Ok();
+        }
+    }
+   
+}

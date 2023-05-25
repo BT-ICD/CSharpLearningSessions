12e5d3fb97ccefe914d7f6c822e6864697f7494a
Bhavin <61580782+BT-ICD@users.noreply.github.com>
2023-05-25 18:58:49 +0530

    Example of custom event and raise event

---
+++ CustomEvent_UserDefineEvent_Demo1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CustomEvent_UserDefineEvent_Demo1;
+
+
+Console.WriteLine("Hello, World!");
+Student s = new Student() ;
+s.SemesterChanged += S_SemesterChanged;
+s.RollNo = 101;
+s.FirstName = "Rajesh";
+s.Semester = 1;
+Console.WriteLine(s);
+s.Semester = 3;
+Console.WriteLine(s);
+void S_SemesterChanged(object? sender, EventArgs e)
+{
+    Console.WriteLine("Semester Changed");
+}
+
+
+
---
+++ CustomEvent_UserDefineEvent_Demo1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace CustomEvent_UserDefineEvent_Demo1
+{
+    internal class Student
+    {
+        public event EventHandler SemesterChanged;
+
+        public int RollNo { get; set; }
+        public string FirstName { get; set; }
+        private int _semester;
+        public int Semester {
+            get{
+            return _semester;
+            } 
+            set
+            {
+                if (value >= 0)
+                {
+                    _semester = value;
+                    SemesterChanged?.Invoke(this, EventArgs.Empty);
+                }
+            } 
+        }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}

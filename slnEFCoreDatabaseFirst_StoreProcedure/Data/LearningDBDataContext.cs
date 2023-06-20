88f4741b63222897eb5b95fd9f70dbad5c2404fb
Bhavin <61580782+BT-ICD@users.noreply.github.com>
2023-06-20 20:12:09 +0530

    DB First EF Core with Store Procedure

---
+++ slnEFCoreDatabaseFirst_StoreProcedure/Data/LearningDBDataContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using slnEFCoreDatabaseFirst_StoreProcedure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace slnEFCoreDatabaseFirst_StoreProcedure.Data
+{
+    internal class LearningDBDataContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-SVLH4LJ; Initial Catalog=LearningDB;User Id=SA; password=SA@123;MultipleActiveResultSets=true;Encrypt=false;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ slnEFCoreDatabaseFirst_StoreProcedure/Program.cs
@@ -0,0 +1,36 @@
+﻿/**
+ * Learning References:
+ * https://learn.microsoft.com/en-us/ef/core/querying/sql-queries
+ * https://www.aspsnippets.com/Articles/Using-Stored-Procedure-in-Entity-Framework-Core-Database-First-Approach.aspx
+ */
+
+
+// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using slnEFCoreDatabaseFirst_StoreProcedure.Data;
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+LearningDBDataContext learningDBDataContext = new LearningDBDataContext();
+
+//To get list of products
+//var products = learningDBDataContext.Products;
+//foreach (var product in products)
+//{
+//    Console.WriteLine(product);
+//}
+
+//To get list of products - From Store Procedure
+//var products = learningDBDataContext.Products.FromSql($"Product_GetAll").ToList();
+////var products = learningDBDataContext.Products.FromSql($"Product_GetAll"); //Cannot access count when it returns IQueryable
+//Console.WriteLine(products.Count); //Able to access when executed as a ToList 
+//foreach (var product in products)
+//{
+//    Console.WriteLine(product);
+//}
+
+//To get a product by Id - From Store Procedure  with Parameter
+var parameterId = new SqlParameter("@Id", 3);
+var product = learningDBDataContext.Products.FromSql($"Product_GetByID {parameterId}").ToList().FirstOrDefault();
+Console.WriteLine(product);

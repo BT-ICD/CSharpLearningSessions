76ad13c2eb7fbb3214eed0e0a8a9264f161606a3
BHAVIN <BHAVIN@DESKTOP-SVLH4LJ>
2023-08-31 20:24:09 +0530

    Example to access details of hostEnvironment

---
+++ Web_DependencyInjection_Example/Controllers/AboutHostingController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_DependencyInjection_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutHostingController : ControllerBase
+    {
+        private readonly IHostEnvironment hostEnvironment;
+
+        public AboutHostingController(IHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+        [HttpGet]
+        public IActionResult GetHostingParams()
+        {
+            var r1 = hostEnvironment.ApplicationName;
+            var r2 = hostEnvironment.IsProduction().ToString();
+            var r3 = hostEnvironment.ContentRootPath;
+            var r4 = hostEnvironment.EnvironmentName;
+            var result = new { ApplicationName = r1 , IsProduction=r2, ContentRootPath =r3, EnvironmentName =r4};
+            return Ok(result);
+
+
+        }
+    }
+}

ce451416f8830fa4081f7b5beee4e1fe003b4a59
BHAVIN <BHAVIN@DESKTOP-SVLH4LJ>
2024-01-25 14:54:45 +0530

    New Project drawing on canvas to meet dimension of HTML Canvas

---
+++ ConsoleApp_To_CreateImageFromCanvasDimension/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using static System.Net.Mime.MediaTypeNames;
+
+Console.WriteLine("Hello, World!");
+// Example dimension data
+int canvasWidth = 745;
+int canvasHeight = 1018;
+
+using (Bitmap canvas = new Bitmap(canvasWidth, canvasHeight))
+{
+    using (Graphics graphics = Graphics.FromImage(canvas))
+    {
+        // Set background color (optional)
+        graphics.Clear(Color.White);
+
+        // Example object dimension data
+        int objectX = 100;
+        int objectY = 200;
+        int objectWidth = 50;
+        int objectHeight = 50;
+        int imageX = 0;
+        int imageY = 0;
+        // Example border dimension data
+        int borderWidth = 2;
+        Color borderColor = Color.Red;
+
+        //Draw Actual Image
+        using (System.Drawing.Image image = System.Drawing.Image.FromFile("1_01.JPG"))
+        {
+            // Draw the image on the canvas
+            graphics.DrawImage(image, imageX, imageY,745,1018.5f);
+          
+        }
+        //Draw 1 marks image
+        using (System.Drawing.Image image = System.Drawing.Image.FromFile("1.png"))
+        {
+            // Draw the image on the canvas
+            graphics.DrawImage(image, 109.453125f, 481, 40, 40);
+
+        }
+        // Draw rectangle border
+        using (Pen borderPen = new Pen(borderColor, borderWidth))
+        {
+            graphics.DrawRectangle(borderPen, 109.453125f, 481, 40, 40);
+        }
+        //Draw 2 marks image
+        using (System.Drawing.Image image = System.Drawing.Image.FromFile("2.png"))
+        {
+            // Draw the image on the canvas
+            graphics.DrawImage(image, 480.609375f, 376, 40, 40);
+        }
+     
+
+        // Draw rectangle border
+        using (Pen borderPen = new Pen(borderColor, borderWidth))
+        {
+            graphics.DrawRectangle(borderPen, 480.609375f, 376, 40, 40);
+        }
+        // Draw a rectangle on the canvas
+        using (Brush brush = new SolidBrush(Color.Blue))
+        {
+            graphics.FillRectangle(brush, objectX, objectY, objectWidth, objectHeight);
+        }
+
+        // You can add more drawing logic for other objects here
+    }
+
+    // Save the canvas to a file (you can return this file path as a response in a real server application)
+    string outputPath = "output.png";
+    canvas.Save(outputPath, ImageFormat.Png);
+
+    Console.WriteLine($"Image saved to: {outputPath}");
+}
--- WebApplicationMVC1/obj/Debug/net5.0/WebApplicationMVC1.RazorTargetAssemblyInfo.cs
+++ WebApplicationMVC1/obj/Debug/net5.0/WebApplicationMVC1.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplicationMVC1")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76ad13c2eb7fbb3214eed0e0a8a9264f161606a3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplicationMVC1.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
